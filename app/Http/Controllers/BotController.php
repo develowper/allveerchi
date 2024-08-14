d42959cda24fa2717fe0b4117391b6211bfa1d82
MMR <moj2raj2@gmail.com>
2024-08-14 19:24:28 +0330

    init project

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -134,7 +134,7 @@ class BotController extends Controller
 //        $CANCEL_REGISTER = "لغو ثبت نام";
 //            Telegram::sendMessage(Telegram::LOGS[0], print_r($update, true));
             if ($text == 'hi') {
-                Telegram::sendMessage($from_id, $update);
+                Telegram::sendMessage($from_id, print_r($update, true));
                 return;
             }
             if ($tc == 'private') {
