3602dd0173237ccf3ba08d0d89ee5c5ccfc912dd
MMR <moj2raj2@gmail.com>
2024-10-12 23:52:56 +0330

    excel export variations

--- resources/js/Components/Cube.vue
+++ resources/js/Components/Cube.vue
@@ -3,7 +3,7 @@
 
     <div class="scene  " id="scene">
       <div class="pivot centered" id="pivot" style="transform: rotateX(-20deg) rotateY(225deg)">
-        <div v-if="true" class="cube " id="cube">
+        <div v-if="false" class="cube " id="cube">
           <div v-for="(piece,idx) in pieces" class="piece " :id="`${piece}${idx+1}`"
                style="transform: rotateX(0deg) translateX(-2em);">
             <div v-for="face in faces" :class="`element ${face}`">
@@ -12,7 +12,7 @@
 
           </div>
         </div>
-        <div v-if="false" class="cube" id="cube">
+        <div v-if="true" class="cube" id="cube">
           <div class="piece   " id="piece1" style="transform: rotateX(0deg) translateX(-2em);">
             <div class="element left ">
               <div class="sticker blue"></div>
