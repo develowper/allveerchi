bde202932c3c6b90e63c2cc594bc9ba1f97dbf7c
MMR <moj2raj2@gmail.com>
2024-10-13 13:55:20 +0330

    excel export variations

--- resources/js/Components/Cube.vue
+++ resources/js/Components/Cube.vue
@@ -542,7 +542,7 @@ export default {
     ass() {
 
       function e(e) {
-        let face = document.createElement("Link");
+        let face = document.createElement("a");
         face.classList += 'text-gray-900 flex items-center justify-center' + " sticker " + colors[e];
         face.setAttribute('href', 'https://ailverchi.ir/shop');
         var text = document.createElement("div");
@@ -570,16 +570,16 @@ export default {
         }
         text.style.transform = `rotateZ(${rotate}deg)`;
         face.appendChild(text);
-        face.addEventListener('click', function (e) {
-              e.preventDefault();
-              window.location = 'https://ailverchi.ir/shop';
-            }, {passive: false}
-        );
-        face.addEventListener('touchstart', function (e) {
-              e.preventDefault();
-              window.location = 'https://ailverchi.ir/shop';
-            }, {passive: false}
-        )
+        // face.addEventListener('click', function (e) {
+        //       e.preventDefault();
+        //       window.location = 'https://ailverchi.ir/shop';
+        //     }, {passive: false}
+        // );
+        // face.addEventListener('touchstart', function (e) {
+        //       e.preventDefault();
+        //       window.location = 'https://ailverchi.ir/shop';
+        //     }, {passive: false}
+        // )
 
         return t += 1 << e, pieces[o].children[e].appendChild(face), "translate" + self.ga(e) + "(" + (e % 2 * 4 - 2) + "em)"
       }
@@ -670,8 +670,8 @@ $faces: (left: (0, -90, 180), right: (0, 90, 90), back: (0, 180, -90), front: (0
 $colors: (blue: #006FB2, green: #1AC54C, white: #F9F7EC, yellow: #FFCF36, orange: #FF5721, red: #FF0030);
 
 html, body {
-  //height: 100%;
-  min-height: -webkit-fill-available;
+  height: 90vh;
+  //min-height: -webkit-fill-available;
   //overflow: hidden;
   //background: $ground-color;
 }
