557516fdc765fa365f16b0f9b148109aeadf3e0e
MMR <moj2raj2@gmail.com>
2024-10-19 12:52:22 +0330

    excel export variations

--- resources/js/Components/Footer.vue
+++ resources/js/Components/Footer.vue
@@ -133,11 +133,12 @@
         </div>
         <div class="h-full ">
           <a referrerpolicy='origin' target='_blank'
-             href='https://trustseal.enamad.ir/?id=537013&Code=OJeOoy3263AWFiCRERbpFgIA9cIaYpa5'><img
+             href='https://trustseal.enamad.ir/?id=537698&Code=YLCFUjBwzyCAft02jdb2iSsDFY10cS0e'><img
               referrerpolicy='origin'
-              src='https://trustseal.enamad.ir/logo.aspx?id=537013&Code=OJeOoy3263AWFiCRERbpFgIA9cIaYpa5' alt=''
-              style='cursor:pointer' code='OJeOoy3263AWFiCRERbpFgIA9cIaYpa5'></a>
+              src='https://trustseal.enamad.ir/logo.aspx?id=537698&Code=YLCFUjBwzyCAft02jdb2iSsDFY10cS0e' alt=''
+              style='cursor:pointer' code='YLCFUjBwzyCAft02jdb2iSsDFY10cS0e'></a>
         </div>
+      
         <div class="h-full ">
           <a referrerpolicy='origin' target='_blank'
              href='https://trustseal.enamad.ir/?id=416945&Code=9HX7V8iOeltU7Fwt8A9Qwi8OHyyvdcCL'><img
