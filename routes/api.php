f9db6da6ae3879330b011722b1f85341ee481b8a
MMR <moj2raj2@gmail.com>
2025-07-20 15:37:31 +0330

    add accesses

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
 Route::post('/bot/drzantia/getupdates', [BotDrZantiaController::class, 'getupdates']);
 Route::post('/bot/drzantia/sendmessage', [BotDrZantiaController::class, 'sendmessage']);
 
