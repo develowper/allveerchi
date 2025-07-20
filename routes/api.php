eb897e4234f4a417a9cf7f87c29572b63e875de9
MMR <moj2raj2@gmail.com>
2025-07-20 15:03:57 +0330

    add accesses

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
 Route::post('/bot/drzantia/getupdates', [BotDrZantiaController::class, 'getupdates']);
 Route::post('/bot/drzantia/sendmessage', [BotDrZantiaController::class, 'sendmessage']);
 
