efe79fb82d8e8a2dd8155a93d1f0af132dbf1125
MMR <moj2raj2@gmail.com>
2025-07-20 17:13:53 +0330

    add accesses

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,6 @@ import {createRequire} from 'node:module';
 const require = createRequire(import.meta.url);
 
 export default defineConfig({
-
     base: '/build/',
     optimizeDeps: {
         exclude: ['leaflet-bing-layer']
