16822d369ea17fd61ae966667516a7a4f69f888e
MMR <moj2raj2@gmail.com>
2025-07-21 15:03:01 +0330

    add accesses

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,6 @@ import {VitePWA} from 'vite-plugin-pwa'
 import {createRequire} from 'node:module';
 
 const require = createRequire(import.meta.url);
-
 export default defineConfig({
     // build: {
     //     outDir: 'public/build',
